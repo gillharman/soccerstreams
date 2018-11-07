516d48894bb7f5a8ddb4795593d6ad52b9850a19
Harman Gill <gillhimmy@gmail.com>
2018-11-06 21:38:21 -0500

    m

--- bin/scheduled_jobs.py
+++ bin/scheduled_jobs.py
@@ -3,11 +3,11 @@ from games.scanner import scanner
 from django_cron import CronJobBase, Schedule
 
 class MyCronJob(CronJobBase):
-    # RUNS_EVERY_MINS = 1
+    RUNS_EVERY_MINS = 1
 
-    # schedule = Schedule(run_every_mins=RUNS_EVERY_MINS)
+    schedule = Schedule(run_every_mins=RUNS_EVERY_MINS)
     code = 'soccerstreams.my_cron_job' # a unique code
 
     def do(selfs):
-        # print("Hello World")
-        scanner.scan_for_games()
+        print("Hello World")
+        # scanner.scan_for_games()
