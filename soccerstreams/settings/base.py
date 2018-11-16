609103e553ec1951d563fff0a260453f7a70f572
Harman Gill <gillhimmy@gmail.com>
2018-11-15 22:07:38 -0500

    m

--- soccerstreams/settings/base.py
+++ soccerstreams/settings/base.py
@@ -107,13 +107,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'games/static'),
 )
 
 # Simplified static file serving.
