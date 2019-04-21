c06dbfede0655e7ddc08753e933cb800bb3ecbed
Harman Gill <gillhimmy@gmail.com>
2019-04-21 09:20:33 -0400

    m

--- soccerstreams/settings/production.py
+++ soccerstreams/settings/production.py
@@ -8,18 +8,18 @@ ALLOWED_HOSTS = ['soccer-streams02.herokuapp.com'] # soccer-streams02.herokuapp.
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'secret'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default' : {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3c592kev84un',
-        'USER': 'rpsjqjgwniikyi',
-        'PASSWORD': '18b25f3eda9c069b14b5b20faaa3310201ba187efceb1098e545044f2cbe91d9',
-        'HOST': 'ec2-54-235-178-189.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
