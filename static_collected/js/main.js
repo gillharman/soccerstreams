a4d32180cf9315d9eb5075e6b3274cc144b7c36c
Harman Gill <gillhimmy@gmail.com>
2019-04-21 09:12:50 -0400

    m

--- static_collected/js/main.js
+++ static_collected/js/main.js
@@ -1,38 +1,22 @@
 $(document).ready(function() {
+    var selected_league = location.pathname.split('/')[1];
+    if(selected_league != '' && selected_league != undefined) {
+        $('#' + selected_league).addClass('active');
+    } else {  // DEFAULT IS PL
+        try {
+            $('#PL').addClass('active');
+        } catch(e) {}
+    }
     var nav_pills = $('#nav-pills ul').children();
     if(nav_pills) {
         for (var i=0; i < nav_pills.length; ++i) {
             $(nav_pills[i]).on('click', function() {
-                $('.nav-pill.active').removeClass('active');
-                $(this).addClass('active');
-                $('#nav-title').text($(this).attr('data-league-name'));
-                $('#match-days-wrapper').remove();
-                $('#wrapper').removeClass('wrapper-outline');
-                $('#wrapper').append("<div class='loader' style='width: 30px; height: 30px; margin: 15px auto;'><div class='is-loading'></div></div>");
-                $.ajax({
-                    url: 'ajax/league_matches',
-                    async: true,
-                    data: {'league' : $(this).attr('data-league')},
-                    success: function(result) {
-                        $('.loader').remove();
-                        $('.is_loading').remove();
-                        $('#wrapper').append(result);
-                        $('#wrapper').addClass('wrapper-outline');
-                        onPageLoad();  // ADD HANDLERS AFTER THE AJAX CALL
-                    },
-                    error: function(result, exception) {
-                        var msg = '';
-                        if(result.responseText) {
-                            msg = result.responseText;
-                        } else {
-                            msg = 'Temporary Unavailable. Please try at a later time.'
-                        }
-                        $('.loader').remove();
-                        $('.is_loading').remove();
-                        $('#wrapper').html(msg);
-                        $('#wrapper').addClass('wrapper-outline');
-                    },
-                })
+                var league = $(this).attr('data-league');
+                if(league == '' || league == undefined) {
+                    window.location = '/';
+                } else {
+                    window.location = "/" + league;
+                }
             })
         }
         onPageLoad();  // ADD HANDLERS AFTER THE FIRST TIME THE PAGE LOAD
@@ -45,3 +29,35 @@ function onPageLoad(){
             window.location = "watch_game/" + $(this).data("match-id")
     })
 }
+
+
+
+// $('#match-days-wrapper').remove();
+
+//                $('#nav-title').text($(this).attr('data-league-name'));
+//                $('#wrapper').removeClass('wrapper-outline');
+//                $('#wrapper').append("<div class='loader' style='width: 30px; height: 30px; margin: 15px auto;'><div class='is-loading'></div></div>");
+//                $.ajax({
+//                    url: 'ajax/league_matches',
+//                    async: true,
+//                    data: {'league' : $(this).attr('data-league')},
+//                    success: function(result) {
+//                        $('.loader').remove();
+//                        $('.is_loading').remove();
+//                        $('#wrapper').append(result);
+//                        $('#wrapper').addClass('wrapper-outline');
+//                        onPageLoad();  // ADD HANDLERS AFTER THE AJAX CALL
+//                    },
+//                    error: function(result, exception) {
+//                        var msg = '';
+//                        if(result.responseText) {
+//                            msg = result.responseText;
+//                        } else {
+//                            msg = 'Temporary Unavailable. Please try at a later time.'
+//                        }
+//                        $('.loader').remove();
+//                        $('.is_loading').remove();
+//                        $('#wrapper').html(msg);
+//                        $('#wrapper').addClass('wrapper-outline');
+//                    },
+//                })
--- static_collected/js/watch_game.js
+++ static_collected/js/watch_game.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
+
     $("#matchup-league").click(function() {
         window.location = "/";
     })
+
+    var links = $("#matchup-nav ul li");
+    for (var i = 0; i < links.length; ++i) {
+        $(links[i]).on("click", function() {
+            $('ul li.tab-active').removeClass('tab-active');
+            $('.tab-content').css('display', 'none');
+
+            var tab_content = $(this).attr('data-tab-content');
+            $('#' + tab_content).css('display','flex');
+            $(this).addClass('tab-active');
+        });
+    }
+
+    $('#add-ace-stream').on("click", function() {
+        $("#ace-stream-form").css("display", "inline-block");
+    });
+
+
+    // ACTIVATE THE LINEUPS TAB
+    $('#default-open').click();
 });
