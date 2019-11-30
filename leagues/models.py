ccf04b6deca87147aa8f1e29fa6b73e12d8ba9ee
Harman Gill <gillhimmy@gmail.com>
2019-11-30 10:43:02 -0500

    model column modification

--- leagues/models.py
+++ leagues/models.py
@@ -5,7 +5,7 @@ class League(models.Model):
     name = models.CharField(max_length=30)
     code = models.CharField(max_length=10)
     country = models.CharField(max_length=30)
-    api_id = models.IntegerField()
+    api_id = models.IntegerField(null=True)
     tracked = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
