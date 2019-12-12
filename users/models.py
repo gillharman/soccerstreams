dbee78eb62d1b7f5ea9707d3ae0a58587de96189
Harman Gill <gillhimmy@gmail.com>
2019-12-12 11:16:15 -0500

    user migrations

--- users/models.py
+++ users/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
-    # created = models.DateTimeField(auto_now_add=True)
-    # updated = models.DateTimeField(auto_now=True)
+    avatar = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
-# class UserAvatar(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     avatar_bytes = models.BinaryField()
-#     name = models.CharField(max_length=140)
-#     width = models.IntegerField()
-#     height = models.IntegerField()
-#     format = models.CharField(max_length=10, null=True)
-#     image_mode = models.CharField(max_length=10)
-#     created = models.DateTimeField(auto_now_add=True)
-#     updated = models.DateTimeField(auto_now=True)
+class UserAvatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    avatar_bytes = models.BinaryField()
+    name = models.CharField(max_length=140)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    format = models.CharField(max_length=10, null=True)
+    image_mode = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
