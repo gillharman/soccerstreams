5c39574008e1d003ef576ef5ffba0c0dd7574268
Harman Gill <gillhimmy@gmail.com>
2019-12-12 11:01:03 -0500

    user migrations

--- users/migrations/0003_delete_useravatar.py
+++ users/migrations/0003_delete_useravatar.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_remove_user_avatar'),
+        ('users', '0001_initial'),
     ]
 
     operations = [
