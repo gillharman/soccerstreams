57350bf3c19e49435827039987e3c3a36ea2a73c
Harman Gill <gillhimmy@gmail.com>
2020-01-20 22:06:10 -0500

    production settings updates

--- soccerstreams/settings/production.py
+++ soccerstreams/settings/production.py
@@ -1,25 +1,24 @@
+import os
+
 from .base import *
 
-# DJANGO_SETTINGS_MODULE="soccerstreams.settings.production"
-
-# ALLOWED_HOSTS = ['soccer-streams02.herokuapp.com'] # soccer-streams02.herokuapp.com
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['soccer-streams02.herokuapp.com']
 
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default' : {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3c592kev84un',
-        'USER': 'rpsjqjgwniikyi',
-        'PASSWORD': '18b25f3eda9c069b14b5b20faaa3310201ba187efceb1098e545044f2cbe91d9',
-        'HOST': 'ec2-54-235-178-189.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': os.environ["DATABASE_ENGINE"],
+        'NAME': os.environ["DATABASE_NAME"],
+        'USER': os.environ["DATABASE_USER"],
+        'PASSWORD': os.environ["DATABASE_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],
+        'PORT': os.environ["DATABASE_PORT"],
     }
 }
