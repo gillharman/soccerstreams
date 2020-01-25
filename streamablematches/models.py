f2e70594f43322aee67b078cb9ef3c5f74e21258
Harman Gill <gillhimmy@gmail.com>
2020-01-25 15:32:28 -0500

    migrations changes

--- streamablematches/models.py
+++ streamablematches/models.py
@@ -4,18 +4,6 @@ from datetime import date
 from matches.models import Match
 
 
-# Create your models here.
-class League(models.Model):
-    name = models.CharField(max_length=30)
-    code = models.CharField(max_length=10)
-    country = models.CharField(max_length=30)
-    api_id = models.IntegerField(null=True)
-    tracked = models.BooleanField(default=False)
-    current_match_day = models.IntegerField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-
 class GamesQuerySet(models.QuerySet):
     def get_games(self, date=date.today()):
         return self.filter(created__date=date).distinct('match')
